d82783ae6b131ade3e1af7dff23605e8b9d0e35f
saridormi <saridormi123@gmail.com>
2021-03-26 13:51:06 +0300

    Fix bugs with repos processing

--- process_repo.py
+++ process_repo.py
@@ -1,7 +1,6 @@
 import os
 from typing import List
-from collections import namedtuple, defaultdict
-from dpu_utils.utils import save_jsonl_gz, RichPath
+from dpu_utils.utils import save_jsonl_gz
 
 from joblib import Parallel, delayed, cpu_count
 
@@ -14,15 +13,18 @@ def process_repo(repo_name, repo_url, file_types=['.java']):
     commit_data_dir = 'extracted_data'
     os.makedirs(commit_data_dir, exist_ok=True)
 
-    if repo_name in os.listdir('temp'): # repo is already cloned
-        repo = pydriller.RepositoryMining(f'temp/{repo_name}', only_no_merge=True,
+    if repo_name + '.jsonl.gz' in os.listdir(commit_data_dir):
+        return
+
+    if repo_name.split('_')[1] in os.listdir('temp'):  # repo is already cloned
+        repo = pydriller.RepositoryMining(f'temp/{repo_name.split("_")[1]}', only_no_merge=True,
                                           only_modifications_with_file_types=file_types)
     else:
         try:
             repo = pydriller.RepositoryMining(repo_url, clone_repo_to='temp', only_no_merge=True,
                                               only_modifications_with_file_types=file_types)
         except:
-            return {}
+            return
         
     commits_data = []
     
@@ -34,7 +36,7 @@ def process_repo(repo_name, repo_url, file_types=['.java']):
         if filter_diff(cur_data['diff']) and filter_msg(cur_data['message']):
             commits_data.append(cur_data)
 
-        if (i + 1) % 100 == 0:
+        if (i + 1) % 1000 == 0:
             print(f"Processed {i + 1} commits in {repo_name}")
     
     save_jsonl_gz(commits_data, os.path.join(commit_data_dir, f'{repo_name}.jsonl.gz'))    
@@ -42,8 +44,10 @@ def process_repo(repo_name, repo_url, file_types=['.java']):
                           
 
 def filter_diff(diff: List[str], min_len=1) -> bool:
-    if len(diff) < min_len:
-        return False              
+    if len(diff) == 0:
+        return False
+    if sum(len(x.split()) for x in diff) < min_len:
+        return False
     return True
                           
 
@@ -56,8 +60,9 @@ def filter_msg(msg: str, min_len=1) -> bool:
 if __name__ == '__main__':
     with open('repos_urls.txt', 'r') as file:
         repo_urls_list = [line.strip() for line in file.readlines()]
+
     with open('repos_names.txt', 'r') as file:
         repo_names_list = [line.strip() for line in file.readlines()]
-        
-with Parallel(cpu_count()) as pool:
-    pool(delayed(process_repo)(repo_name, repo_url) for repo_name, repo_url in zip(repo_names_list, repo_urls_list))    
+
+    with Parallel(cpu_count()) as pool:
+        pool(delayed(process_repo)(repo_name, repo_url) for repo_name, repo_url in zip(repo_names_list, repo_urls_list))
